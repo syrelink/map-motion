a4aea49ca7f77a39c04013a2be366cbb924d8c53
syr-macmin <syrelink@gmail.com>
2025-10-16 15:51:03 +0800

    ff

--- scripts/t2m_contact_motion/train_ddp.sh
+++ scripts/t2m_contact_motion/train_ddp.sh
@@ -19,6 +19,6 @@ CUDA_VISIBLE_DEVICES=0,1 torchrun --nnodes=1 --nproc_per_node=2 --rdzv_backend=c
             task.train.save_every_step=100000 \
             task.dataset.train_transforms=['RandomEraseLang','RandomEraseContact','NumpyToTensor'] \
             model=cmdm \
-            model.arch='trans_DCA' \
+            model.arch='trans_wkv' \
             model.data_repr='h3d' \
             model.text_model.max_length=20
