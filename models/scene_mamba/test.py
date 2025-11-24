2fa304610e398a57d27394d680262d56e044e5a7
syr-macmin <syrelink@gmail.com>
2025-11-24 13:42:33 +0800

    del

--- models/scene_mamba/test.py
+++
@@ -1,4 +0,0 @@
-import torch
-print(f"PyTorch version: {torch.__version__}")
-print(f"PyTorch was built with CUDA version: {torch.version.cuda}")
-print(f"Is CUDA available for PyTorch? {torch.cuda.is_available()}")
